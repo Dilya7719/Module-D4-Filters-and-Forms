efcd1f0b0e10988645a2b2f4a925ae5885ab6d68
Dilya <dkhamidullina@schneider-consult.ru>
2023-06-13 00:25:02 +0500

    Init

---
+++ NewsPortal/news/views.py
@@ -0,0 +1,81 @@
+from django.urls import reverse_lazy
+
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+
+from .models import Post, Author
+from .filters import PostFilter
+from .forms import PostForm
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'Posts'
+    ordering = '-post_create_date'
+    paginate_by = 10
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'post'
+
+
+class PostsList_with_search(ListView):
+    model = Post
+    template_name = 'news_search.html'
+    context_object_name = 'Posts'
+    ordering = '-post_create_date'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class NewsCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.post_type = 'NW'
+        return super().form_valid(form)
+
+
+class ArticlesCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class NewsUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list_search')
+
+
+class ArticlesUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class ArticlesDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list_search')
